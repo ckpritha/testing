16771f497e05456cf4e7948fc6a2233d75207710
ckpri <ckpri@DESKTOP-A8JGTP9.home>
2021-07-08 09:35:13 -0400

    connecting jenkins

---
+++ testing/src/testing/fisrt.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class fisrt {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("connection successful"); 
+		System.out.println("Connecting jenkins");
+	}
+}
